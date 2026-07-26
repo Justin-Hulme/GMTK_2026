ee7c57a6c380ff870f69d271b1ec045a6810e613
Tevin Coburn <coburntevin@gmail.com>
2026-07-26 06:41:02 -0600

    fixed coin bug and merge conflict

--- Coin/coin.gd
+++ Coin/coin.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.add_score(value * 10)
+		body.add_score(value)
 		coin_picked_up.emit(body.get_score())
 		visible = false
 		pickup_sound.play()
