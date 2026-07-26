ce6fb09ef76f6eae08cbf3a6cd1787ee6c1e2fab
mr.josher <mr.josher@gmail.com>
2026-07-25 19:46:31 -0600

    player sprite changes based on angle

--- Player/player.gd
+++ Player/player.gd
@@ -31,39 +31,49 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_phone"):
 		phone_screen.toggle()
 
+func _get_facing_name(angle_rad: float) -> String:
+	var angle := rad_to_deg(angle_rad)
+	if angle < 0.0:
+		angle += 360.0
+	if angle >= 337.5 or angle < 22.5:
+		return "east"
+	elif angle < 67.5:
+		return "south_east"
+	elif angle < 112.5:
+		return "south"
+	elif angle < 157.5:
+		return "south_west"
+	elif angle < 202.5:
+		return "west"
+	elif angle < 247.5:
+		return "north_west"
+	elif angle < 292.5:
+		return "north"
+	else:
+		return "north_east"
+
 func _physics_process(delta: float) -> void:
 	var mouse_position := get_global_mouse_position()
 	var direction := mouse_position - global_position
 
-	# Rotate character toward mouse
-	rotation = direction.angle() + deg_to_rad(90)
-
 	var target_velocity := Vector2.ZERO
 
-	# Move toward cursor while holding left mouse button
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		target_velocity = direction.normalized() * speed
 
-	# Accelerate/decelerate smoothly
 	if target_velocity.length() > 0:
-		velocity = velocity.move_toward(
-			target_velocity,
-			acceleration * delta
-		)
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
 	else:
-		velocity = velocity.move_toward(
-			Vector2.ZERO,
-			deceleration * delta
-		)
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 
 	move_and_slide()
 
-	# Animation
+	var facing := _get_facing_name(direction.angle())
 	if velocity.length() > 10:
-		animated_sprite.play("walking")
+		animated_sprite.play("walk_" + facing)
 	else:
-		animated_sprite.play("idle")
-		
+		animated_sprite.play("idle_" + facing)
+
 func add_score(amount: int) -> void:
 	score += amount
 	set_coin_total(score)
